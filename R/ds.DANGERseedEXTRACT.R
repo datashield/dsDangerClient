baf944240d584def60b21128d3d1183680fe3fa6
Stuart Wheater <stuart.wheater@arjuna.com>
2019-07-08 13:42:45 +0100

    Added 'ds.DANGERseedEXTRACT', plus other updates

---
+++ R/ds.DANGERseedEXTRACT.R
@@ -0,0 +1,17 @@
+
+ds.DANGERseedEXTRACT <- function(datasources=NULL){
+  
+  # details are provided look for 'opal' objects in the environment
+  if(is.null(datasources)){
+    datasources <- findLoginObjects()
+  }
+  
+  calltext <- call('DANGERseedEXTRACTDS')
+  
+  extract.seed <- opal::datashield.aggregate(datasources, calltext)
+  
+  return(extract.seed=extract.seed)
+  
+}
+
+# ds.DANGERseedEXTRACT
